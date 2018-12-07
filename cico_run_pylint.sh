2a387bd71e8517beda068706203f20fb0748b11d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-07 09:50:20 +0100

    Check cyclomatic complexity and maintainability index on CI

--- cico_run_pylint.sh
+++ cico_run_pylint.sh
@@ -9,4 +9,8 @@ prep() {
 }
 
 prep
+./detect-common-errors.sh
+./detect-dead-code.sh
+./measure-cyclomatic-complexity.sh --fail-on-error
+./measure-maintainability-index.sh --fail-on-error
 ./run-linter.sh
